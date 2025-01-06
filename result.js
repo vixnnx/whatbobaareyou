0481ef26c87cbcd23e68dfa532f0109472fbe511
vixnnx <122330789+vixnnx@users.noreply.github.com>
2025-01-06 16:58:17 -0500

    Update result.js

--- result.js
+++ result.js
@@ -95,23 +95,23 @@ if (temp === "A") {
 		document.getElementById("bobaimg").src = "pictures/hottea.PNG";
 		document.getElementById("boba").innerHTML = "Hot Tea";
 		document.getElementById("desc").innerHTML = "You are a go with the flow type of person.<br>You are willing to sacrifice for your friends.<br>You easily trust in others, but that is not always a good thing.";
-	} else if ((B === 2 && A === 3) || (A === 2 && B === 3) || (A === 3 && B === 3)) {
+	} else if ((B === 2 && A === 3) || (A === 2 && B === 3) || (A === 3 && B === 3) || (A === 2 && B === 2 && C === 1)) {
 		document.getElementById("bobaimg").src = "pictures/brownsugar.PNG";
 		document.getElementById("boba").innerHTML = "Brown Sugar Boba";
 		document.getElementById("desc").innerHTML = "You know when to have fun and be silly, and when it's time to be serious.<br>You try to plan ahead, but you always fail and do something impulsive instead.<br>You might not have a lot of friends, but you have good ones.";
-	} else if ((C === 2 && A === 3) || (A === 2 && C === 3) || (A === 3 && C === 3) ) { 
+	} else if ((C === 2 && A === 3) || (A === 2 && C === 3) || (A === 3 && C === 3) || (A === 2 && C === 2 && B === 1)) { 
 		document.getElementById("bobaimg").src = "pictures/passionfruit.PNG";
 		document.getElementById("boba").innerHTML = "Passion Fruit Boba";
 		document.getElementById("desc").innerHTML = "To most people, you seem very chill.<br>But your friends know that you are stressed out.<br>You try to get things done but you fail.";
-	} else if ((D === 2 && A === 3) || (A === 2 && D === 3) || (A === 3 && D === 3)) {
+	} else if ((D === 2 && A === 3) || (A === 2 && D === 3) || (A === 3 && D === 3) || (A === 2 && D === 2 && B === 1)) {
 		document.getElementById("bobaimg").src = "pictures/honeydew.PNG";
 		document.getElementById("boba").innerHTML = "Honeydew Boba";
 		document.getElementById("desc").innerHTML = "You try to go with the flow, but every once in a while you lock in.<br>You love your friends, but sometimes you need a break.";
-	} else if ((C === 2 && B === 3) || (B === 2 && C === 3) || (B === 3 && C === 3)) {
+	} else if ((C === 2 && B === 3) || (B === 2 && C === 3) || (B === 3 && C === 3) || (B === 2 && C === 2 && A === 1)) {
 		document.getElementById("bobaimg").src = "pictures/thai.PNG";
 		document.getElementById("boba").innerHTML = "Thai Tea";
 		document.getElementById("desc").innerHTML = "You like to have fun, but you get tired easily.<br>Sometimes you just need a break.";
-	} else if ((B === 2 && D === 3) || (D === 2 && B === 3) || (D === 3 && B === 3)) {
+	} else if ((B === 2 && D === 3) || (D === 2 && B === 3) || (D === 3 && B === 3) || (D === 2 && B === 2) && A === 1) {
 		document.getElementById("bobaimg").src = "pictures/rose.PNG";
 		document.getElementById("boba").innerHTML = "Rose Tea";
 		document.getElementById("desc").innerHTML = "You want to help but sometimes you get lazy.<br>You enjoy life and want to experience it all, but from your couch.";
