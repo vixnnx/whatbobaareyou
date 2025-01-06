37f117890ee39c2714b4b604f3d93ff0a6f76de5
vixnnx <122330789+vixnnx@users.noreply.github.com>
2025-01-05 23:14:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -33,32 +33,79 @@ if (question === 1) {
 }
 
 document.body.style.backgroundColor = "lightpink";
-console.log(localStorage.getItem("Q1"));
+
 console.log(localStorage.getItem("Q2"));
 console.log(localStorage.getItem("Q3"));
 console.log(localStorage.getItem("Q4"));
-console.log(localStorage.getItem("Q5"));
-console.log(localStorage.getItem("Q6"));
 console.log(localStorage.getItem("Q7"));
 console.log(localStorage.getItem("Q8"));
-console.log(localStorage.getItem("Q9"));
 
 
 
 
 document.getElementById("choice1").onclick = function () {
+	if (question === 2) {
+		localStorage.setItem("Q2", "A");
+	} else if (question === 3) {
+		localStorage.setItem("Q3", "A");
+	} else if (question === 4) {
+		localStorage.setItem("Q4", "A");
+	} else if (question === 7) {
+		localStorage.setItem("Q7", "A");
+	} else if (question === 8) {
+		localStorage.setItem("Q8", "A");
+	}
 	location.href = nextQuestion;	
 };
 
 document.getElementById("choice2").onclick = function () {
+	if (question === 2) {
+		localStorage.setItem('Q2', "B");
+	} else if (question === 3) {
+		localStorage.setItem("Q3", "B");
+	} else if (question === 4) {
+		localStorage.setItem("Q4", "B");
+	} else if (question === 7) {
+		localStorage.setItem("Q7", "B");
+	} else if (question === 8) {
+		localStorage.setItem("Q8", "B");
+	} else if (question === 9) {
+		localStorage.setItem("Q9", "B");
+	}
 	location.href = nextQuestion;	
 };
 
 document.getElementById("choice3").onclick = function () {
+	if (question === 2) {
+		localStorage.setItem("Q2", "C");
+	} else if (question === 3) {
+		localStorage.setItem("Q3", "C");
+	} else if (question === 4) {
+		localStorage.setItem("Q4", "C");
+	} else if (question === 7) {
+		localStorage.setItem("Q7", "C");
+	} else if (question === 8) {
+		localStorage.setItem("Q8", "C");
+	} else if (question === 9) {
+		localStorage.setItem("Q9", "C");
+	}
 	location.href = nextQuestion;
 };
 
 document.getElementById("choice4").onclick = function () {
+	if (question === 2) {
+		localStorage.setItem("Q2", "D");
+	} else if (question === 3) {
+		localStorage.setItem("Q3", "D");
+	} else if (question === 4) {
+		localStorage.setItem("Q4", "D");;
+	} else if (question === 7) {
+		localStorage.setItem("Q7", "D");
+	} else if (question === 8) {
+		localStorage.setItem("Q8", "D");
+	}else if (question === 9) {
+		localStorage.setItem("Q9", "D");
+	}
 	location.href = nextQuestion;
 }; 
 
